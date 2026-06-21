b5fc8618bc49fc6dbde2936166bcaa613158edb5
fmansoor-03 <fawazarshad30@gmail.com>
2026-06-21 14:16:50 -0600

    implemeneted fuzzy metals

--- src/main.cc
+++ src/main.cc
@@ -13,8 +13,8 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3); // Use a non-zero fuzz value to make the left sphere appear blurry
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0); // Use a fuzz value of 1.0 to make the right sphere appear very blurry
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
