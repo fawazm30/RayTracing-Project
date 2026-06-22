eb55b29f8969256424c7e4d124065d085135baaa
fmansoor-03 <fawazarshad30@gmail.com>
2026-06-21 18:21:48 -0600

    playing around with camera positions

--- src/main.cc
+++ src/main.cc
@@ -13,9 +13,9 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.50); // Use a refraction index of 1.50 to make the left sphere appear like a glass bubble
-    auto material_bubble = make_shared<dielectric>(1.00 / 1.50); // Use the inverse of the left sphere's refraction index to make a hollow bubble inside the left sphere
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0); // Use a fuzz value of 1.0 to make the right sphere appear very blurry
+    auto material_left   = make_shared<dielectric>(1.50);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
@@ -25,10 +25,17 @@ int main() {
 
     camera cam;
 
+    // Configure the camera with a 16:9 aspect ratio, 400 pixel width, and 100 samples per pixel.
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width  = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50; // Allow rays to bounce up to 50 times into the scene
+
+    // Configure the camera to look from point (-2, 2, 1) towards the center of the scene at (0, 0, -1), with a vertical field of view of 90 degrees and a camera-relative "up" direction of (0, 1, 0).
+    cam.vfov     = 90; // change field of view
+    cam.lookfrom = point3(-2,2,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
 
     cam.render(world);
 }
